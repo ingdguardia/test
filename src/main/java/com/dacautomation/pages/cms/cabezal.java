322707be0f11f17becb25e555126b4e7c0161f2e
ingdguardia <rimuu48@gmail.com>
2024-12-02 07:23:59 -0300

    push

--- src/main/java/com/dacautomation/pages/cms/cabezal.java
+++ src/main/java/com/dacautomation/pages/cms/cabezal.java
@@ -1,122 +1,109 @@
-//package com.dacautomation.pages.cms;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//public class cabezal {
-//
-//    public void createHeader(WebDriver driver, String nombreCabezal, String contenido, String url, String archivo,
-//                             String caracteristicas) throws IOException {
-//
-//        try {
-//            System.out.println("##############CREAR CABEZAL##############");
-//            click(driver, dropdownHomePortal);
-//            clickJS(driver, btnCabezales);
-//            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//            driver.switchTo().window(tabs.get(1));
-//            click(driver, btnNuevo);
-//            tabs = new ArrayList<String>(driver.getWindowHandles());
-//            driver.switchTo().window(tabs.get(2));
-//            Thread.sleep(3000);
-//            sendKeys(driver, tituloAdjunto, nombreCabezal);
-//            driver.switchTo().frame("texto1iframe");
-//            sendKeys(driver, inputCuerpoNota, contenido);
-//            driver.switchTo().parentFrame();
-//            sendKeys(driver, inputUrlCabezal, url);
-//            sendKeys(driver, inputNombreAdjuntoBanner, archivo);
-//            Thread.sleep(2000);
-//            WebElement wElement = driver.findElement(By.xpath("(//input[starts-with(@onclick,'FilesList')])[1]"));
-//            idImagen = wElement.getAttribute("value");
-//            System.out.println("VALOR: " + idImagen);
-//            checkboxAdjunto = By.xpath("//*[@id='FileList_" + idImagen + "_item']/figure/div/label/input");
-//            Thread.sleep(2000);
-//            clickJS(driver, checkboxAdjunto);
-//            Thread.sleep(2000);
-//            clickJS(driver, btnRelacionarNota);
-//            Thread.sleep(2000);
-//            sendKeys(driver, inputCaracteristicasCabezal, caracteristicas);
-//            optionCaracteristicasCabezal = By.xpath("//li//span[text()='" + caracteristicas + "']");
-//            visibiltyOf(driver, optionCaracteristicasCabezal);
-//            clickJS(driver, optionCaracteristicasCabezal);
-//            chipCaracteristicaCabezal = By
-//                    .xpath("//div[@class='chip__body']//span[text()='" + caracteristicas + "']");
-//            visibiltyOf(driver, chipCaracteristicaCabezal);
-//            clickJS(driver, btnGrabar);
-//            presenceOf(driver, alertGuardado);
-//            driver.navigate().refresh();
-//            Thread.sleep(3000);
-//            // VERIFICAR
-//            chipCaracteristicaCabezal = By
-//                    .xpath("//span[text()=' " + caracteristicas + " ']");
-//            checkValueElement(driver, tituloAdjunto, nombreCabezal);
-//            inputCuerpoNota = By.xpath("//*[@id='mce_0']//p[text()='" + contenido + "']");
-//            driver.switchTo().frame("texto1iframe");
-//            elementIsClickable(driver, inputCuerpoNota);
-//            driver.switchTo().parentFrame();
-//            checkValueElement(driver, inputUrlCabezal, url);
-//            visibiltyOf(driver, selectedArchivo);
-//            visibiltyOf(driver, chipCaracteristicaCabezal);
-//            driver.close();
-//            driver.switchTo().window(tabs.get(1));
-//            sendKeys(driver, inputFiltrar, nombreCabezal);
-//            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            headerObjeto = setHeaderNote(nombreCabezal);
-//            visibiltyOf(driver, headerObjeto);
-//            driver.close();
-//            driver.switchTo().window(tabs.get(0));
-//            System.out.println("##############CREAR CABEZAL OK##############");
-//        } catch (Exception e) {
-//            errorColor(driver, "##############NO SE PUDO CREAR EL CABEZAL: " + nombreCabezal + " ERROR: " + e, e);
-//            screenshot(driver, "ERROR CREAR " + nombreCabezal);
-//
-//        }
-//
-//    }
-//
-//    public void deleteHeader(WebDriver driver, String nombreCabezal) throws IOException {
-//        try {
-//            System.out.println("##############BORRANDO CABEZAL##############");
-//            System.out.println(driver.getCurrentUrl());
-//            Thread.sleep(5000);
-//            if (driver.getCurrentUrl().equals(url + "administrator/cabezales")) {
-//                System.out.println(url + "administrator/cabezales");
-//                System.out.println("##############SECCION CABEZAL OK##############");
-//                sendKeys(driver, inputFiltrar, nombreCabezal);
-//                driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            } else {
-//                System.out.println("##############CLICK SECCION CABEZAL##############");
-//                click(driver, dropdownHomePortal);
-//                click(driver, btnCabezales);
-//                ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//                tabs = new ArrayList<String>(driver.getWindowHandles());
-//                driver.switchTo().window(tabs.get(1));
-//                sendKeys(driver, inputFiltrar, nombreCabezal);
-//                driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            }
-//            setHeaderNote(nombreCabezal);
-//            click(driver, headerObjeto);
-//            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//            tabs = new ArrayList<String>(driver.getWindowHandles());
-//            driver.switchTo().window(tabs.get(2));
-//            Thread.sleep(5000);
-//            clickJS(driver, btnInfo);
-//            clickJS(driver, btnEliminar);
-//            clickJS(driver, btnSi);
-//            driver.switchTo().window(tabs.get(1));
-//            sendKeys(driver, inputFiltrar, nombreCabezal);
-//            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            visibiltyOf(driver, lblNotFound);
-//            driver.close();
-//            driver.switchTo().window(tabs.get(0));
-//            System.out.println("##############CABEZAL BORRADO##############");
-//        } catch (Exception e) {
-//            errorColor(driver, "ERROR, NO SE PUDO BORRAR EL CABEZAL " + nombreCabezal + " RAZON: " + e, e);
-//            screenshot(driver, "ERROR BORRAR " + nombreCabezal);
-//        }
-//    }
-//}
+package com.dacautomation.pages.cms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class cabezal extends cms_base{
+
+    public cabezal(WebDriver _driver) {
+        super(_driver);
+    }
+
+    public void createHeader(WebDriver driver, String nombreCabezal, String contenido, String url, String archivo,
+                             String caracteristicas,String idImagen, String test) throws IOException {
+        By inputCuerpoNota = By.xpath("//*[@id='mce_0']/p");
+
+        try {
+            click(driver, dropdownHomePortal,"CLICK DROPDOWN HOMEPORTAL",test);
+            clickJS(driver, btnCabezales,"CLICK BTN CABEZALES",test);
+            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(1));
+            click(driver, btnNuevo,"CLICK BTN NUEVO",test);
+            tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(2));
+            Thread.sleep(3000);
+            sendKeys(driver, tituloAdjunto, nombreCabezal,"TITULO ADJUNTO",test);
+            driver.switchTo().frame("texto1iframe");
+            sendKeys(driver, inputCuerpoNota, contenido,"INPUT CUERPO CABEZAL",test);
+            driver.switchTo().parentFrame();
+            sendKeys(driver, inputUrlCabezal, url,"INPUT URL CABEZAL",test);
+            sendKeys(driver, inputNombreAdjuntoBanner, archivo,"",test);
+            Thread.sleep(2000);
+            WebElement wElement = driver.findElement(By.xpath("(//input[starts-with(@onclick,'FilesList')])[1]"));
+            idImagen = wElement.getAttribute("value");
+            checkboxAdjunto = By.xpath("//*[@id='FileList_" + idImagen + "_item']/figure/div/label/input");
+            Thread.sleep(2000);
+            clickJS(driver, checkboxAdjunto,"CLICK CHECKBOX",test);
+            Thread.sleep(2000);
+            clickJS(driver, btnRelacionarNota,"CLICK BTN RELACIONAR",test);
+            Thread.sleep(2000);
+            sendKeys(driver, inputCaracteristicasCabezal, caracteristicas,"",test);
+            optionCaracteristicasCabezal = By.xpath("//li//span[text()='" + caracteristicas + "']");
+            visibilityOf(driver, optionCaracteristicasCabezal,test);
+            clickJS(driver, optionCaracteristicasCabezal,"CLICK",test);
+            chipCaracteristicaCabezal = By.xpath("//div[@class='chip__body']//span[text()='" + caracteristicas + "']");
+            visibilityOf(driver, chipCaracteristicaCabezal,test);
+            clickJS(driver, btnGrabar,"CLICK",test);
+            presenceOf(driver, toastGuardado,test);
+            driver.navigate().refresh();
+            Thread.sleep(3000);
+            // VERIFICAR
+            chipCaracteristicaCabezal = By.xpath("//span[text()=' " + caracteristicas + " ']");
+            checkValueElement(driver, tituloAdjunto, nombreCabezal,test);
+            inputCuerpoNota = By.xpath("//*[@id='mce_0']//p[text()='" + contenido + "']");
+            driver.switchTo().frame("texto1iframe");
+            esClickeable(driver, inputCuerpoNota,test);
+            driver.switchTo().parentFrame();
+            checkValueElement(driver, inputUrlCabezal, url,test);
+            visibilityOf(driver, selectedArchivo,test);
+            visibilityOf(driver, chipCaracteristicaCabezal,test);
+            driver.close();
+            driver.switchTo().window(tabs.get(1));
+            sendKeys(driver, inputFiltrar, nombreCabezal,"",test);
+            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
+            headerObjeto = setHeaderNote(nombreCabezal);
+            visibilityOf(driver, headerObjeto,test);
+            driver.close();
+            driver.switchTo().window(tabs.get(0));
+            SELECTCOLOR(test,"[OK]: CREAR CABEZAL");
+        } catch (Exception e) {
+            errorColor(driver, "NO SE PUDO CREAR EL CABEZAL", e,test);
+        }
+
+    }
+
+    public void deleteHeader(WebDriver driver, String nombreCabezal, String url, String test) throws IOException {
+        try {
+
+            click(driver, dropdownHomePortal,"CLICK DROPDOWN HOME PORTAL",test);
+            click(driver, btnCabezales,"CLICK BTN CABEZALES",test);
+            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+            tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(1));
+            sendKeys(driver, inputFiltrar, nombreCabezal,"INPUT FILTRAR",test);
+            setHeaderNote(nombreCabezal);
+            click(driver, headerObjeto,"CLICK HEADER OBJETO",test);
+            tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(2));
+            Thread.sleep(5000);
+            clickJS(driver, btnInfo,"CLICK BTN INFO",test);
+            clickJS(driver, btnEliminar,"CLICK BTN ELIMINAR",test);
+            clickJS(driver, btnSi,"CLICK BTN SI",test);
+            driver.switchTo().window(tabs.get(1));
+            sendKeys(driver, inputFiltrar, nombreCabezal,"INPUT FILTRAR",test);
+            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
+            visibilityOf(driver, lblNotFound,test);
+            driver.close();
+            driver.switchTo().window(tabs.get(0));
+            SELECTCOLOR(test,"[OK]: BORRAR CABEZAL");
+
+        } catch (Exception e) {
+            errorColor(driver, "NO SE PUDO BORRAR EL CABEZAL ", e,test);
+        }
+    }
+}
--- src/main/java/com/dacautomation/pages/cms/encuesta.java
+++ src/main/java/com/dacautomation/pages/cms/encuesta.java
@@ -1,97 +1,84 @@
-//package com.dacautomation.pages.cms;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebDriver;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//public class encuesta extends cms_base {
-//
-//    public encuesta(WebDriver _driver) {
-//        super(_driver);
-//    }
-//
-//    public void createSurvey(WebDriver driver, String nombreEncuesta, String opcion1, String opcion2, String opcion3)
-//            throws IOException {
-//        try {
-//            System.out.println("##############CREAR ENCUESTA##############");
-//            click(driver, btnEncuesta);
-//            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//            driver.switchTo().window(tabs.get(1));
-//            click(driver, btnNuevo);
-//            tabs = new ArrayList<String>(driver.getWindowHandles());
-//            driver.switchTo().window(tabs.get(2));
-//            Thread.sleep(3000);
-//            sendKeys(driver, tituloAdjunto, nombreEncuesta);
-//            sendKeys(driver, inputOpcionesEncuesta, opcion1);
-//            driver.findElement(inputOpcionesEncuesta).sendKeys(Keys.ENTER);
-//            optionEncuesta = By.xpath("//span[text()='" + opcion1 + "']");
-//            visibiltyOf(driver, optionEncuesta);
-//            sendKeys(driver, inputOpcionesEncuesta, opcion2);
-//            driver.findElement(inputOpcionesEncuesta).sendKeys(Keys.ENTER);
-//            optionEncuesta = By.xpath("//span[text()='" + opcion2 + "']");
-//            sendKeys(driver, inputOpcionesEncuesta, opcion3);
-//            driver.findElement(inputOpcionesEncuesta).sendKeys(Keys.ENTER);
-//            optionEncuesta = By.xpath("//span[text()='" + opcion3 + "']");
-//            clickJS(driver, btnGrabar);
-//            Thread.sleep(3000);
-//            driver.close();
-//            driver.switchTo().window(tabs.get(1));
-//            headerObjeto = setHeaderNote(nombreEncuesta);
-//            sendKeys(driver, inputFiltrar, nombreEncuesta);
-//            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            visibiltyOf(driver, headerObjeto);
-//            driver.close();
-//            driver.switchTo().window(tabs.get(0));
-//            System.out.println("##############CREAR ENCUESTA OK##############");
-//        } catch (Exception e) {
-//            errorColor(driver, "ERROR, NO SE PUDO CREAR LA ENCUESTA " + nombreEncuesta + " RAZON: " + e, e);
-//            screenshot(driver, "ERROR CREAR " + nombreEncuesta);
-//        }
-//
-//    }
-//
-//    public void deleteSurvey(WebDriver driver, String nombreEncuesta) throws IOException {
-//        try {
-//            System.out.println("##############BORRANDO ENCUESTA##############");
-//            System.out.println(driver.getCurrentUrl());
-//            Thread.sleep(5000);
-//            if (driver.getCurrentUrl().equals(url + "administrator/encuestas")) {
-//                System.out.println(url + "administrator/encuestas");
-//                System.out.println("##############SECCION ENCUESTA OK##############");
-//                sendKeys(driver, inputFiltrar, nombreEncuesta);
-//                driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            } else {
-//                System.out.println("##############CLICK SECCION ENCUESTA##############");
-//                click(driver, btnEncuesta);
-//                ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//                tabs = new ArrayList<String>(driver.getWindowHandles());
-//                driver.switchTo().window(tabs.get(1));
-//                sendKeys(driver, inputFiltrar, nombreEncuesta);
-//                driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            }
-//            setHeaderNote(nombreEncuesta);
-//            click(driver, headerObjeto);
-//            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
-//            tabs = new ArrayList<String>(driver.getWindowHandles());
-//            driver.switchTo().window(tabs.get(2));
-//            Thread.sleep(5000);
-//            clickJS(driver, btnInfo);
-//            clickJS(driver, btnEliminar);
-//            clickJS(driver, btnSi);
-//            driver.switchTo().window(tabs.get(1));
-//            sendKeys(driver, inputFiltrar, nombreEncuesta);
-//            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
-//            visibiltyOf(driver, lblNotFound);
-//            driver.close();
-//            driver.switchTo().window(tabs.get(0));
-//            System.out.println("##############ENCUESTA BORRADA##############");
-//        } catch (Exception e) {
-//            errorColor(driver, "ERROR, NO SE PUDO  BORRAR LA ENCUESTA " + nombreEncuesta + " RAZON: " + e, e);
-//            screenshot(driver, "ERROR BORRAR " + nombreEncuesta);
-//        }
-//    }
-//
-//}
+package com.dacautomation.pages.cms;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class encuesta extends cms_base {
+
+    public encuesta(WebDriver _driver) {
+        super(_driver);
+    }
+
+    public void createSurvey(WebDriver driver, String nombreEncuesta, String opcion1, String opcion2, String opcion3, String test)
+            throws IOException {
+        try {
+            click(driver, btnEncuesta,"CLICK BTN ENCUESTA",test);
+            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(1));
+            click(driver, btnNuevo,"CLICK BTN NUEVO",test);
+            tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(2));
+            Thread.sleep(3000);
+            sendKeys(driver, tituloAdjunto, nombreEncuesta,"INPUT TITULO ENCUESTA",test);
+            sendKeys(driver, inputOpcionesEncuesta, opcion1,"INPUT OPCION 1 ENCUESTA",test);
+            driver.findElement(inputOpcionesEncuesta).sendKeys(Keys.ENTER);
+            optionEncuesta = By.xpath("//span[text()='" + opcion1 + "']");
+            visibilityOf(driver, optionEncuesta,test);
+            sendKeys(driver, inputOpcionesEncuesta, opcion2,"INPUT OPCION 2 ENCUESTA",test);
+            driver.findElement(inputOpcionesEncuesta).sendKeys(Keys.ENTER);
+            optionEncuesta = By.xpath("//span[text()='" + opcion2 + "']");
+            sendKeys(driver, inputOpcionesEncuesta, opcion3,"INPUT OPCION 3 ENCUESTA",test);
+            driver.findElement(inputOpcionesEncuesta).sendKeys(Keys.ENTER);
+            optionEncuesta = By.xpath("//span[text()='" + opcion3 + "']");
+            clickJS(driver, btnGrabar,"CLICK BTN GRABAR",test);
+            Thread.sleep(3000);
+            driver.close();
+            driver.switchTo().window(tabs.get(1));
+            headerObjeto = setHeaderNote(nombreEncuesta);
+            sendKeys(driver, inputFiltrar, nombreEncuesta,"INPUT NOMBRE ENCUESTA",test);
+            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
+            visibilityOf(driver, headerObjeto,test);
+            driver.close();
+            driver.switchTo().window(tabs.get(0));
+            SELECTCOLOR(test,"[OK]: CREAR ENCUESTA");
+
+        } catch (Exception e) {
+            errorColor(driver, "NO SE PUDO CREAR LA ENCUESTA" ,e,test);
+        }
+
+    }
+
+    public void deleteSurvey(WebDriver driver, String nombreEncuesta,String url, String test) throws IOException {
+        try {
+
+            click(driver, btnEncuesta,"CLICK BTN ENCUESTA",test);
+            ArrayList<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+            tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(1));
+            sendKeys(driver, inputFiltrar, nombreEncuesta,"",test);
+            setHeaderNote(nombreEncuesta);
+            click(driver, headerObjeto,"CLICK HEADER OBJETO",test);
+            tabs = new ArrayList<String>(driver.getWindowHandles());
+            driver.switchTo().window(tabs.get(2));
+            Thread.sleep(5000);
+            clickJS(driver, btnInfo,"CLICK BTN INFO",test);
+            clickJS(driver, btnEliminar,"CLICK BTN ELIMINAR",test);
+            clickJS(driver, btnSi,"CLICK BTN SI",test);
+            driver.switchTo().window(tabs.get(1));
+            sendKeys(driver, inputFiltrar, nombreEncuesta,"INPUT FILTRAR",test);
+            driver.findElement(inputFiltrar).sendKeys(Keys.ENTER);
+            visibilityOf(driver, lblNotFound,test);
+            driver.close();
+            driver.switchTo().window(tabs.get(0));
+            SELECTCOLOR(test,"[OK]: BORRAR ENCUESTA");
+
+        } catch (Exception e) {
+            errorColor(driver, "NO SE PUDO  BORRAR LA ENCUESTA", e,test);
+        }
+    }
+
+}
